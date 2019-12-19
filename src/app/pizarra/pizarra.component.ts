9547be42c551e0538c381bebf2ce2257b79739bc
Pablo Massimino <pmassimino@hotmail.com>
2019-12-19 11:44:58 -0300

    regular commit

--- src/app/pizarra/pizarra.component.ts
+++ src/app/pizarra/pizarra.component.ts
@@ -16,7 +16,7 @@ export class PizarraComponent implements OnInit {
   data: cotizacionCereal[] = [];
   mercadoData:Mercado[] =[]
   isLoadingResults = true;
-  pizarraData:pizarraCereal=new pizarraCereal();
+  pizarraData:pizarraCereal=new pizarraCereal();  
 
   constructor(private api: ApiService,private mercadoApi:MercadoService) { }
 
@@ -42,8 +42,8 @@ export class PizarraComponent implements OnInit {
   transformData(data:cotizacionCereal[])
   {
      this.pizarraData.id_mercado = data.find(d=>d.id_Cereal=="23").id_Mercado;
-     this.pizarraData.fecha = data.find(d=>d.id_Cereal=="23").fecha;
-     this.pizarraData.fecha = data.find(d=>d.id_Cereal=="23").fecha;
+     this.pizarraData.fecha = new Date();
+     //this.pizarraData.fecha = new Date(); //data.find(d=>d.id_Cereal=="23").fecha;
      this.pizarraData.soja = data.find(d=>d.id_Cereal=="23").precio;
      this.pizarraData.maiz = data.find(d=>d.id_Cereal=="19").precio;
      this.pizarraData.sorgo = data.find(d=>d.id_Cereal=="22").precio;
@@ -61,6 +61,49 @@ export class PizarraComponent implements OnInit {
     cotizacion.precio = this.pizarraData.soja;
     cotizacion.fecha = this.pizarraData.fecha;
     this.api.insert(cotizacion).subscribe();
+//Maiz
+    cotizacion = new cotizacionCereal();
+    id = uuid();
+    cotizacion.id = id.substr(0,10);
+    cotizacion.id_Cereal = "19";
+    cotizacion.id_Mercado = this.pizarraData.id_mercado;
+    cotizacion.id_Moneda = "PES";
+    cotizacion.precio = this.pizarraData.maiz;
+    cotizacion.fecha = this.pizarraData.fecha;
+    this.api.insert(cotizacion).subscribe();
+    //Trigo
+    cotizacion = new cotizacionCereal();
+    id = uuid();
+    cotizacion.id = id.substr(0,10);
+    cotizacion.id_Cereal = "15";
+    cotizacion.id_Mercado = this.pizarraData.id_mercado;
+    cotizacion.id_Moneda = "PES";
+    cotizacion.precio = this.pizarraData.trigo;
+    cotizacion.fecha = this.pizarraData.fecha;
+    this.api.insert(cotizacion).subscribe();
+    //girasol
+    cotizacion = new cotizacionCereal();
+    id = uuid();
+    cotizacion.id = id.substr(0,10);
+    cotizacion.id_Cereal = "02";
+    cotizacion.id_Mercado = this.pizarraData.id_mercado;
+    cotizacion.id_Moneda = "PES";
+    cotizacion.precio = this.pizarraData.girasol;
+    cotizacion.fecha = this.pizarraData.fecha;
+    this.api.insert(cotizacion).subscribe();
+    //sorgo
+    cotizacion = new cotizacionCereal();
+    id = uuid();
+    cotizacion.id = id.substr(0,10);
+    cotizacion.id_Cereal = "22";
+    cotizacion.id_Mercado = this.pizarraData.id_mercado;
+    cotizacion.id_Moneda = "PES";
+    cotizacion.precio = this.pizarraData.sorgo;
+    cotizacion.fecha = this.pizarraData.fecha;
+    this.api.insert(cotizacion).subscribe();
+
+
+
 
   }
   
